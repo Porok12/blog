01b95ad424c487a4f21f1a24606a07ccc994cfd0
przemek <przemekpapla@gmail.com>
2023-07-01 23:23:56 +0200

    test deployment

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -5,25 +5,14 @@ interface Props {
     reset: () => void
 }
 
-const GlobalError = ({error, reset}: Props) => {
+const GlobalError = ({ error, reset }: Props) => {
     return (
-        <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-            <div className="text-center">
-                <p className="text-base font-semibold text-indigo-600">500</p>
-                <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Something went
-                    wrong!</h1>
-                <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking
-                    for.</p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
-                    <button
-                        onClick={() => reset()}
-                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        Try again
-                    </button>
-                </div>
-            </div>
-        </main>
+        <html>
+            <body>
+                <h2>Something went wrong!</h2>
+                <button onClick={() => reset()}>Try again</button>
+            </body>
+        </html>
     )
 }
 
