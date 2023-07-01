d4b488045021966604173fd0dfd3de7415ed84c6
przemek <przemekpapla@gmail.com>
2023-07-01 23:18:33 +0200

    minor changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
-const About = () => {
-  return (
-      <div className="py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          Hello
+import {NextPage} from "next";
+
+const About: NextPage = () => {
+    return (
+        <div className="py-24 sm:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                Hello
+            </div>
         </div>
-      </div>
-  )
+    )
 }
 
 export default About;
