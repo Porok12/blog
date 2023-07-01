fc2f48a89375406ab81b0dd1b34c863158ce2f95
przemek <przemekpapla@gmail.com>
2023-07-01 22:59:41 +0200

    add initial about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+      <div className="py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          Hello
+        </div>
+      </div>
+  )
+}
+
+export default About;
