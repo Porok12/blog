9800dddf04aefcbc1c546d4b6534cd8540d5982f
przemek <przemekpapla@gmail.com>
2023-07-02 11:04:39 +0200

    another article

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import {NextPage} from "next";
 import Link from "next/link";
 
-const Post = (props: any) => {
+interface Props {
+    slug: string;
+    data: {
+        title: string;
+        date: string;
+        datetime?: string;
+        description: string;
+        categories: string;
+    }
+}
+
+const Post: NextPage<Props> = (props) => {
     const {slug, data} = props;
+
+    const categories = data.categories.split(",");
 
     return (
         <article className="flex max-w-xl flex-col items-start justify-between">
@@ -10,12 +24,15 @@ const Post = (props: any) => {
                 <time dateTime={data.datetime} className="text-gray-500">
                     {data.date}
                 </time>
-                <Link
-                    href={`tags/${slug}`}
-                    className="relative z-10 rounded-full px-3 py-1.5 font-medium bg-gray-100 dark:bg-slate-600 text-gray-600 dark:text-white hover:bg-gray-100"
-                >
-                    {data.categories}
-                </Link>
+                {categories.map(category => (
+                    <Link
+                        key={category}
+                        href={`tags/${slug}`}
+                        className="relative z-10 rounded-full px-3 py-1.5 font-medium bg-gray-100 dark:bg-slate-600 text-gray-600 dark:text-white hover:bg-gray-100"
+                    >
+                        {category}
+                    </Link>
+                ))}
             </div>
             <div className="group relative">
                 <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
