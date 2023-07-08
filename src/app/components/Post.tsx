50d5a3893c1f192b78fa528bceca1a77030c89c4
przemek <przemekpapla@gmail.com>
2023-07-08 14:11:39 +0200

    fix compilation

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -41,7 +41,6 @@ const Post: NextPage<Props> = (props) => {
                 src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"
                 className="rounded-2xl"
                 style={{width: '100%', height: 'auto'}}
-                fill
             />
 
             <div className="mx-2 mt-4 flex-grow">
