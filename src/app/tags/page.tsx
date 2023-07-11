a3e29f7701784bf6d555de45ef9c5b7feb488a49
przemek <przemekpapla@gmail.com>
2023-07-11 20:51:17 +0200

    tags with counter

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,25 +1,39 @@
+import Border from '@/app/components/Border'
 import React from 'react'
 import {NextPage} from 'next'
 import CustomLink from '@/app/components/CustomLink'
 import Chips from '@/app/components/Chips'
 
 const getData = async () => {
-  const tags: string[] = await fetch('https://dev.to/api/articles?username=porok12')
+  return await fetch('https://dev.to/api/articles?username=porok12')
     .then(response => response.json())
     .then(articles => articles.map(article => article.tags))
     .then((tags: string[]) => tags.flatMap(tags => tags.split(',')))
-  return tags
+    .then((tags: string[]) => tags.reduce((acc, curr) => {
+      const index = acc.findIndex(tag => tag.name === curr)
+      if (index >= 1) {
+        acc[index].count += 1
+        return acc
+      } else {
+        return [...acc, { name: curr, count: 1 }]
+      }
+    }, [] as {name: string, count: number}[]))
 }
 
 const Tags: NextPage = async () => {
   const tags = await getData()
   return (
     <>
-      <h2 className="text-3xl tracking-tight">Tags</h2>
-      <div className="mt-4">
+      <h2 className="mb-6 text-center text-3xl font-light tracking-tighter sm:text-left sm:text-5xl">
+        Tags
+      </h2>
+
+      <Border/>
+
+      <div className="mt-12">
         {tags.map(tag => (
-          <CustomLink key={tag} as={Chips} href={`tags/${tag}`}>
-            {tag}
+          <CustomLink key={tag.name} as={Chips} href={`/tags/${tag.name}`}>
+            {tag.name} {tag.count}
           </CustomLink>
         ))}
       </div>
