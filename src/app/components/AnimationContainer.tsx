863004ef68d60bff1645dcd58a777dcc844bd181
przemek <przemekpapla@gmail.com>
2023-08-05 09:28:20 +0200

    about-page: new component for animated section

--- src/app/components/AnimationContainer.tsx
+++ src/app/components/AnimationContainer.tsx
@@ -1,6 +1,8 @@
-import {m, useInView, useScroll, useTransform, Variants} from 'framer-motion'
-import React, {CSSProperties, useEffect, useRef, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
+import {m, useInView, useScroll, Variants} from 'framer-motion'
 import Stack from '@/app/components/Stack'
+
+const ANIMATION_STEPS = 9
 
 const variants: Record<number, Variants> = ({
   1: {
@@ -131,22 +133,45 @@ const variants: Record<number, Variants> = ({
   },
 })
 
+const SlideContent = (props: any) => {
+  const {
+    active = false,
+    style = {},
+    axis = 'y',
+    title = 'Title',
+    children = undefined,
+  } = props
+
+  return (
+    <m.div
+      initial="hidden"
+      animate={active ? 'active' : 'hidden'}
+      variants={{
+        hidden: { [axis]: -100, opacity: 0 },
+        active: { [axis]: 0, opacity: 1 },
+      }}
+      style={{ position: 'fixed', ...style }}
+    >
+      {typeof title === 'string' ? <div className="mb-4 text-3xl">{title}</div> : title}
+      {children && <div className="text-base">{children}</div>}
+    </m.div>
+  )
+}
+
 const AnimationContainer = () => {
   const ref = useRef(null)
   const isInView = useInView(ref, {amount: 'some', margin: '0px'})
   const {scrollYProgress} = useScroll({
     target: ref,
-    offset: ['start end', 'end start'],
+    offset: ['start end', 'end end'],
   })
 
   const [state, setState] = useState(0)
   useEffect(() => {
     return scrollYProgress.on('change', v => {
-      const ticks = 9
-      const length  = 1 / ticks
-      setState(Math.floor(v / length))
+      const stepLength  = 1 / ANIMATION_STEPS
+      setState(Math.floor(v / stepLength))
     })
-    // return scrollYProgress.on('change', v=> setHookedYPosition(v))
   }, [scrollYProgress])
 
   // const [display, setDisplay] = useState(false)
@@ -157,133 +182,44 @@ const AnimationContainer = () => {
   return (
     <div
       ref={ref}
-      className="flex,justify-center h-[3000px] w-full my-64"
+      className="flex,justify-center my-64 h-[3000px] w-full"
     >
       <span className="fixed left-0 top-10">{state}</span>
 
 
-      <m.div
-        initial="hidden"
-        animate={state > 1 && state < 9 ? 'active' : 'hidden'}
-        variants={{
-          hidden: {
-            y: -100,
-            opacity: 0,
-          },
-          active: {
-            y: 0,
-            opacity: 1,
-          },
-        }}
-        style={{
-          position: 'fixed',
-          top: '10%',
-          right: '50%',
-          marginRight: '-80px',
-        }}
-      >
-        <h2 className="my-4 text-3xl">Tech Stack</h2>
-      </m.div>
+      <SlideContent
+        title={<h2 className="my-4 text-3xl">Tech Stack</h2>}
+        active={state > 1 && state < 9}
+        style={{ top: '10%', right: '50%', marginRight: '-80px' }}
+      />
 
-      <m.div
-        initial="hidden"
-        animate={state === 3 ? 'active' : 'hidden'}
-        variants={{
-          hidden: {
-            y: -100,
-            opacity: 0,
-          },
-          active: {
-            y: 0,
-            opacity: 1,
-          },
-        }}
-        // className="fixed right-0 top-0 mt-[50%]"
-        style={{
-          position: 'fixed',
-          top: '50%',
-          right: '25%',
-          marginTop: '-100px',
-          // marginLeft: '-250px',
-        }}
-      >
-        <div className="text-3xl mb-4">DevOps</div>
-        <div className="text-base mb-8">
-          Hello world!
-        </div>
-        <div className="flex gap-4">
+      <SlideContent title="DevOps" active={state === 3} style={{ top: '50%', right: '25%', marginTop: '-100px' }}>
+        Hello world!
+        <div className="flex gap-4 mt-4">
           <i className="devicon-kubernetes-plain" style={{fontSize: 64, color: '#326de6'}}></i>
           <i className="devicon-docker-plain" style={{fontSize: 64, color: '#239ced'}}></i>
           <i className="devicon-linux-plain" style={{fontSize: 64, color: '#e8e8e8'}}></i>
         </div>
-      </m.div>
+      </SlideContent>
 
-      <m.div
-        initial="hidden"
-        animate={state === 5 ? 'active' : 'hidden'}
-        variants={{
-          hidden: {
-            y: -100,
-            opacity: 0,
-          },
-          active: {
-            y: 0,
-            opacity: 1,
-          },
-        }}
-        // className="fixed right-0 top-0 mt-[50%]"
-        style={{
-          position: 'fixed',
-          top: '50%',
-          left: '25%',
-          marginTop: '-100px',
-          // marginLeft: '-350px',
-        }}
-      >
-        <div className="text-3xl mb-4">Backend</div>
-        <div className="text-base mb-8">
-          Hello world!
-        </div>
-        <div className="flex gap-4">
+      <SlideContent title="Backend" active={state === 5} style={{ top: '50%', left: '25%', marginTop: '-100px' }}>
+        Hello world!
+        <div className="flex gap-4 mt-4">
           <i className="devicon-scala-plain" style={{fontSize: 80, color: '#de3423'}}/>
           <i className="devicon-java-plain" style={{fontSize: 80, color: '#e76f00'}}></i>
           <i className="devicon-cplusplus-plain" style={{fontSize: 80, color: '#0181ce'}}></i>
         </div>
-      </m.div>
+      </SlideContent>
 
-      <m.div
-        initial="hidden"
-        animate={state === 7 ? 'active' : 'hidden'}
-        variants={{
-          hidden: {
-            y: -100,
-            opacity: 0,
-          },
-          active: {
-            y: 0,
-            opacity: 1,
-          },
-        }}
-        // className="fixed right-0 top-0 mt-[50%]"
-        style={{
-          position: 'fixed',
-          top: '50%',
-          right: '25%',
-          marginTop: '-100px',
-          // marginLeft: '-150px',
-        }}
-      >
-        <div className="text-3xl mb-4">Frontend</div>
-        <div className="text-base mb-8">
-          Hello world!
-        </div>
-        <div className="flex gap-4">
+      <SlideContent title="Frontend" active={state === 7} style={{ top: '50%', right: '25%', marginTop: '-100px' }}>
+        Hello world!
+        <div className="flex gap-4 mt-4">
           <i className="devicon-react-plain" style={{fontSize: 64, color: '#61dafb'}}></i>
           <i className="devicon-nextjs-plain" style={{fontSize: 64, color: '#efefef'}}></i>
           <i className="devicon-angularjs-plain" style={{fontSize: 64, color: '#dd1b16'}}></i>
           <i className="devicon-typescript-plain" style={{fontSize: 64, color: '#007acc'}}></i>
         </div>
-      </m.div>
+      </SlideContent>
 
       <Stack
         initial="0"
--- src/app/components/ScrollProgress.tsx
+++ src/app/components/ScrollProgress.tsx
@@ -1,7 +1,7 @@
-import {easeIn, easeInOut, m, useScroll, useSpring, useTransform} from 'framer-motion'
+import {easeInOut, m, useScroll, useSpring, useTransform} from 'framer-motion'
 
 const ScrollProgress = () => {
-  const { scrollYProgress } = useScroll()
+  const {scrollYProgress} = useScroll()
   const scaleY = useTransform(scrollYProgress, [0, 0.1], [0, 1], {ease: easeInOut})
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
@@ -12,7 +12,7 @@ const ScrollProgress = () => {
   return (
     <m.div
       className="fixed top-0 z-10 h-1 w-full bg-indigo-600"
-      style={{ scaleX, scaleY }}
+      style={{scaleX, scaleY}}
     />
   )
 }
