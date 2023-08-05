9d5fffe6c180b9cab64e7fd0a39c3a07589c0d72
przemek <przemekpapla@gmail.com>
2023-08-05 23:09:07 +0200

    cache :(

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import Border from '@/app/components/Border'
 import ArticleApi, {IArticle} from '@/api/articles'
 
 // export const dynamic = 'force-dynamic'
-export const revalidate = 0
+export const revalidate = 60
+// export const fetchCache = 'force-no-store'
 
 
 const getData = async () => {
