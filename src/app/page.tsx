44bfea3bb3d260384c0732bd27a9f584f819a7ac
przemek <przemekpapla@gmail.com>
2023-08-05 22:57:56 +0200

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import Article from '@/app/components/Article'
 import Border from '@/app/components/Border'
 import ArticleApi, {IArticle} from '@/api/articles'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
+
 const getData = async () => {
   try {
     return await ArticleApi.articles()
